33f0509a0e46abdee36ec2d059ae267e1c03716d
Sam Inniss <sjinniss1@hotmail.com>
2016-03-09 21:04:41 -0500

    testing imports

--- main/libs.js
+++ main/libs.js
@@ -12,10 +12,10 @@ requirejs.config({
 //requirejs(['d3.min','jquery','../main/md3']);
 try
 {
-	requirejs(['libr/d3.min','libr/jquery','main/md3']);
+	requirejs(['lib/d3.min','lib/jquery','main/md3']);
 	//requirejs(['md3/lib/d3.min','md3/lib/jquery.min','md3/main/md3']);
 }
 catch (err)
 {
-	requirejs(['md3/libr/d3.min','md3/libr/jquery.min','md3/main/md3']);
+	requirejs(['md3/lib/d3.min','md3/lib/jquery.min','md3/main/md3']);
 }
