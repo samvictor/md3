470f93dd49d74d0d12363652b86b8998d9bea560
Sam Inniss <sjinniss1@hotmail.com>
2016-03-09 22:59:42 -0500

    fixing imports

--- main/libs.js
+++ main/libs.js
@@ -1,25 +1,15 @@
 // Configure loading modules from the lib directory
+
 requirejs.config({
-    //baseUrl: '../lib',
-	baseUrl: '../md3/lib',
-	//baseUrl: '..',
+	baseUrl: '../lib',   		// for local
+	//baseUrl: '../md3/lib',   	// for github
+	enforceDefine: true,
 	paths: {
-		//jquery: "jquery.min"
-		'req_jquery': "http://code.jquery.com/jquery-2.2.1.min",
-		'req_d3': "http://d3js.org/d3.v3.min"
+		jquery: "http://code.jquery.com/jquery-2.2.1.min",
+		'req_d3': "http://d3js.org/d3.v3.min",
+		'req_md3': ['../md3/main/md3', '../main/md3']
 	}
 });
 
 // Start loading the main app file
-//requirejs(['../libr/d3.min','../libr/jquery.min','../main/md3']);
-requirejs(['req_d3', 'req_jquery', '../main/md3']);
-//requirejs(['d3.min','jquery','../main/md3']);
-/*try
-{
-	requirejs(['lib/d3.min','lib/jquery','main/md3']);
-	//requirejs(['md3/lib/d3.min','md3/lib/jquery.min','md3/main/md3']);
-}
-catch (err)
-{
-	requirejs(['md3/lib/d3.min','md3/lib/jquery.min','md3/main/md3']);
-}*/
+requirejs(['req_d3', 'jquery', 'req_md3']);
--- main/md3.js
+++ main/md3.js
@@ -18,7 +18,8 @@ document.head.appendChild(md3_style);
 
 // Require.js
 //define(['../libr/jquery.min', '../libr/d3.min'], function ( $, d3 ) {
-define(['req_jquery', 'req_d3'], function ( $, d3 ) {
+define(['jquery', 'req_d3'], function ( $, d3 ) {
+	
 	var w_w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	var w_h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 	var margin = {top: 10, right: 6, bottom: 20, left: 60}
