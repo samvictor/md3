6006c7da4aeec930fc1856ebe7fbe5d39e896aaa
Sam Inniss <sjinniss1@hotmail.com>
2016-03-09 20:00:16 -0500

    fixing imports

--- main/libs.js
+++ main/libs.js
@@ -10,4 +10,11 @@ requirejs.config({
 
 // Start loading the main app file
 //requirejs(['d3.min','jquery','../main/md3']);
-requirejs(['lib/d3.min','lib/jquery','main/md3','md3/lib/d3.min','md3/lib/jquery','md3/main/md3']);
+try
+{
+	requirejs(['lib/d3.min','lib/jquery','main/md3']);
+}
+catch (err)
+{
+	requirejs(['md3/lib/d3.min','md3/lib/jquery','md3/main/md3']);
+}
