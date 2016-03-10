1186a015674a35984f04d1cc253de0489f8d2e64
Sam Inniss <sjinniss1@hotmail.com>
2016-03-09 20:12:27 -0500

    testing imports

---
+++ main/libs.js
@@ -0,0 +1,21 @@
+// Configure loading modules from the lib directory
+requirejs.config({
+    //baseUrl: '../lib',
+	//baseUrl: '../md3/lib',
+	baseUrl: '..',
+	paths: {
+		//jquery: "jquery.min"
+	}
+});
+
+// Start loading the main app file
+//requirejs(['d3.min','jquery','../main/md3']);
+try
+{
+	//requirejs(['lib/d3.min','lib/jquery','main/md3']);
+	requirejs(['md3/lib/d3.min','md3/lib/jquery.min','md3/main/md3']);
+}
+catch (err)
+{
+	requirejs(['md3/lib/d3.min','md3/lib/jquery.min','md3/main/md3']);
+}
