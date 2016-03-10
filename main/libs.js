93162edcfed9d49d523c4910312a143593cce65f
Sam Inniss <sjinniss1@hotmail.com>
2016-03-09 22:08:20 -0500

    testing imports

--- main/libs.js
+++ main/libs.js
@@ -4,12 +4,15 @@ requirejs.config({
 	baseUrl: '../md3/lib',
 	//baseUrl: '..',
 	paths: {
-		jquery: "jquery.min"
+		//jquery: "jquery.min"
+		req_jquery: "http://code.jquery.com/jquery-1.12.0.min.js"
+		req_d3: "http://d3js.org/d3.v3.min.js"
 	}
 });
 
 // Start loading the main app file
-requirejs(['../libr/d3.min','../libr/jquery.min','../main/md3']);
+//requirejs(['../libr/d3.min','../libr/jquery.min','../main/md3']);
+requirejs(['req_d3', 'req_jquery', '../main/md3']);
 //requirejs(['d3.min','jquery','../main/md3']);
 /*try
 {
