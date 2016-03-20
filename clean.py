e40c32f047b114c5150fe7e3af004f3fe9ddda33
Sam Inniss <sjinniss1@hotmail.com>
2016-03-20 19:19:38 -0400

    creating clean.py to format data for MD3

---
+++ clean.py
@@ -0,0 +1,93 @@
+# Cleans raw data and makes it appropriate for other parts of MD3
+# This includes adding unique ids like "row_1" to every row, 
+# and expanding variables with letters into other dimensions with 1 or 0
+# as the values
+
+
+import csv, sys
+
+# Get raw data
+datafile = "cars.csv"
+
+# set to True if first column entries are labels
+has_labels = False
+
+if len(sys.argv) > 1:
+    datafile = sys.argv[1]
+    if len(sys.argv) > 2:
+        if sys.argv == "has_labels":
+            has_labels = True
+
+
+row_begin = 0
+if has_labels:
+    row_begin = 1
+    
+data = []
+to_expand = []
+with open(datafile, 'rb') as csvfile:
+    data_read = csv.reader(csvfile, delimiter=',')
+    for j, row in enumerate(data_read):
+        for i in range(row_begin, len(row)):
+            try:
+                row[i] = float(row[i])
+            except:
+                if j: # if j != 0, which is where the labels are
+                    row[i] = temp_cell = row[i].strip()
+                    if temp_cell == '':
+                        row[i] = 0
+                        print "Warning: blank entry found, 0 used instead"
+                    else:
+                        if not i in to_expand:
+                            to_expand.append(i)
+                    
+        data.append(row)
+
+        
+# expand string data into different dimensions
+for i in to_expand:
+    unique_strings = []
+    for j, row in enumerate(data):
+        if j == 0:
+            continue
+        
+        if row[i] not in unique_strings:
+            unique_strings.append(row[i])
+        
+    for j, row in enumerate(data):
+        if j == 0:
+            continue
+            
+        build_row = []
+        for s in unique_strings:
+            if row[i] == s:
+                build_row.append(1)
+            else:
+                build_row.append(0)
+        
+        row += build_row
+    unique_strings = map(lambda s: s+"_col_"+str(i), unique_strings)
+    data[0] += unique_strings
+
+to_expand.sort(reverse = True)
+
+# remove originals with strings from data
+for row in data:
+    for j in to_expand:
+        del row[j]
+
+# add "row_{row num}" and key "md3_id"
+data[0] = ["md3_id"] + data[0]
+for i in range(1, len(data)):
+    data[i] = ["row_"+str(i)] + data[i]
+
+
+for row in data:
+    print row
+    
+#print data
+# file name should only have one dot
+#with open(datafile.split('.')[0]+'_clean.'+datafile.split('.')[1], 'wb') as fixed_file:
+#    fixed_writer = csv.writer(fixed_file, delimiter=',')
+#    for r in data:
+#        fixed_writer.writerow(r)
--- md3.py
+++ md3.py
@@ -4,10 +4,8 @@
 import numpy, csv, numbers, ast, json
 from sklearn import manifold, datasets, preprocessing
 from sklearn.decomposition import PCA, NMF, FastICA, FactorAnalysis
-# scikits-learn
-# dimension reduction
+
 # iris data set
-# car data set - syntagmatic.github.io/parallel-coordinates
 # drag select
 
 # matplotlib
@@ -18,40 +16,31 @@ from sklearn.decomposition import PCA, NMF, FastICA, FactorAnalysis
 letter_data = []
 letter_label = ""
 with open('cars.csv', 'rb') as csvfile:
-	data_read = csv.reader(csvfile, delimiter=',')
-	for row in data_read:
-		for i in range(1, len(row)):
-			try:
-				row[i] = ast.literal_eval(row[i])
-			except:
-				if row[i] == '':
-					row[i] = 0
-				pass
-		letter_data.append(row)
-
-letter_data[0] = ["md3_id"] + letter_data[0]
-for i in range(1, len(letter_data)):
-	letter_data[i] = ["row_"+str(i)] + letter_data[i]
-
-with open('md3_data.csv', 'wb') as fixed_file:
-	fixed_writer = csv.writer(fixed_file, delimiter=',')
-	for r in letter_data:
-		fixed_writer.writerow(r)
+    data_read = csv.reader(csvfile, delimiter=',')
+    for row in data_read:
+        for i in range(1, len(row)):
+            try:
+                row[i] = ast.literal_eval(row[i])
+            except:
+                if row[i] == '':
+                    row[i] = 0
+                pass
+        letter_data.append(row)
 
 # Some useful functions
 def strip_labels(data):
-	ret_data = []
-	for row in data[2:]:
-		ret_data.append(row[2:])
-	return ret_data
+    ret_data = []
+    for row in data[2:]:
+        ret_data.append(row[2:])
+    return ret_data
 
 # for car data
 def add_labels(coords, original):
-	combined = []
-	for i in range(0, len(coords)):
-		combined.append({"label":original[i+1][0], "pretty_label":original[i+1][1],
-											"x":coords[i,0], "y":coords[i,1]})
-	return combined
+    combined = []
+    for i in range(0, len(coords)):
+        combined.append({"label":original[i+1][0], "pretty_label":original[i+1][1],
+                                            "x":coords[i,0], "y":coords[i,1]})
+    return combined
 
 
 # Scikit learn
@@ -67,44 +56,44 @@ n_points = 1000
 X, color = datasets.samples_generator.make_s_curve(n_points, random_state=0)
 
 #for i in range(0, len(X)):
-#	X[i][0] *= 1000
+#    X[i][0] *= 1000
 #print X
 
 def add_labels (coords, original):
-	combined = []
-	for i in range(0, len(coords)):
-		combined.append({"label": "row_"+str(i), "pretty_label": str(i),
-											"x":coords[i,0], "y":coords[i,1]})
-	return combined
+    combined = []
+    for i in range(0, len(coords)):
+        combined.append({"label": "row_"+str(i), "pretty_label": str(i),
+                                            "x":coords[i,0], "y":coords[i,1]})
+    return combined
 
 def add_labels2 (coords):
-	combined = []
-	for i in range(0, len(coords)):
-		combined.append({"label": "row_"+str(i), "pretty_label": str(i),
-											"x":coords[i,0], "y":coords[i,1], "z":coords[i,2]})
-	return combined
+    combined = []
+    for i in range(0, len(coords)):
+        combined.append({"label": "row_"+str(i), "pretty_label": str(i),
+                                            "x":coords[i,0], "y":coords[i,1], "z":coords[i,2]})
+    return combined
 
-def make_csv(sphere_data):	
-	sphere_data = add_labels2(sphere_data)
-	with open('sphere_data.csv', 'wb+') as sphere_file:
-		sphere_writer = csv.writer(sphere_file, delimiter=',')
-		
-		sphere_writer.writerow(['md3_id', 'name', 'x', 'y', 'z'])
-		
-		amount = 398.0
-		data_size = 0.0
-		data_size = len(sphere_data)
-		step = 0.0
-		step = data_size/amount
-		print len(sphere_data)
-		count = 0
-		
-		for i in range(0, int(len(sphere_data)/step)):
-			r = sphere_data[i*int(step)]
-			sphere_writer.writerow([r['label'], r['pretty_label'], r['x'], r['y'], r['z']])
-			count += 1
-		
-		print count
+def make_csv(sphere_data):    
+    sphere_data = add_labels2(sphere_data)
+    with open('sphere_data.csv', 'wb+') as sphere_file:
+        sphere_writer = csv.writer(sphere_file, delimiter=',')
+        
+        sphere_writer.writerow(['md3_id', 'name', 'x', 'y', 'z'])
+        
+        amount = 398.0
+        data_size = 0.0
+        data_size = len(sphere_data)
+        step = 0.0
+        step = data_size/amount
+        print len(sphere_data)
+        count = 0
+        
+        for i in range(0, int(len(sphere_data)/step)):
+            r = sphere_data[i*int(step)]
+            sphere_writer.writerow([r['label'], r['pretty_label'], r['x'], r['y'], r['z']])
+            count += 1
+        
+        print count
 
 make_csv(X)
 """
@@ -123,7 +112,7 @@ colors = p[indices]
 
 sp_x, sp_y, sp_z = numpy.sin(t[indices]) * numpy.cos(p[indices]), \
     numpy.sin(t[indices]) * numpy.sin(p[indices]), \
-    numpy.cos(t[indices])	
+    numpy.cos(t[indices])    
 
 X = numpy.array([sp_x, sp_y, sp_z]).T
 #print X
@@ -137,7 +126,7 @@ X = numpy.transpose(X)
 range_scaler = preprocessing.MinMaxScaler()
 
 for i in range(0, len(X)):
-	X[i] = range_scaler.fit_transform(X[i])
+    X[i] = range_scaler.fit_transform(X[i])
 
 
 X = numpy.transpose(X)
@@ -170,11 +159,11 @@ tsne_out = tsne.fit_transform(X)
 
 # names need to be unique (pretty names don't)
 to_file = [{"name": "mds", "p_name":"MDS", "data": add_labels(mds_out, letter_data)},
-			{"name": "pca", "p_name":"PCA", "data": add_labels(pca_out, letter_data)},
-			{"name": "nmf", "p_name":"NMF", "data": add_labels(nmf_out, letter_data)},
-			{"name": "ica", "p_name":"Fast ICA", "data": add_labels(ica_out, letter_data)},
-			{"name": "fa", "p_name":"Factor Analysis", "data": add_labels(fa_out, letter_data)},
-			{"name": "tsne", "p_name":"T-SNE", "data": add_labels(tsne_out, letter_data)}]
+            {"name": "pca", "p_name":"PCA", "data": add_labels(pca_out, letter_data)},
+            {"name": "nmf", "p_name":"NMF", "data": add_labels(nmf_out, letter_data)},
+            {"name": "ica", "p_name":"Fast ICA", "data": add_labels(ica_out, letter_data)},
+            {"name": "fa", "p_name":"Factor Analysis", "data": add_labels(fa_out, letter_data)},
+            {"name": "tsne", "p_name":"T-SNE", "data": add_labels(tsne_out, letter_data)}]
 
 # create json file
 json_file = open('main/data.json', 'w+')
