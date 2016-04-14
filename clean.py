60e72670d71fc173271230c9cd0a2e036fe2d1be
Sam Inniss <sjinniss1@hotmail.com>
2016-04-14 18:24:18 -0400

    Cleaning up the bars a little

--- clean.py
+++ clean.py
@@ -12,56 +12,72 @@ datafile = "cars.csv"
 # set to True if first column entries are labels
 has_labels = False
 ignore_cols = []
+label_col = 0
+limit_output = False
 
 if len(sys.argv) > 1:
     datafile = sys.argv[1]
     if len(sys.argv) > 2:
         if sys.argv == "has_labels":
             has_labels = True
+        
 
 # for financial data
-#ignore_cols = [14, 1]
-
-has_labels = False
+ignore_cols = [14, 1]
+has_labels = True
 label_col = 0
-limit_output = False
-
-row_begin = 0
-if has_labels:
-    row_begin = 1
+limit_output = True
     
 data = []
 to_expand = []
 header = []
+ignored_head_count = 0
+ignored_count = 0
 with open(datafile, 'rb') as csvfile:
     data_read = csv.reader(csvfile, delimiter=',') # change back to comma
+    
     for j, row in enumerate(data_read):
+        new_row = []
         if j == 0:
-            header = row
-            
+            for i, element in enumerate(row):
+                if i not in ignore_cols:
+                    new_row.append(element)
+                else:
+                    ignored_head_count += 1
+                
+            header = new_row
+            data.append(new_row)
+            continue
+        
         #for i in range(row_begin, len(row)):
-        for i in range(row_begin, len(row)):
+        for i, element in enumerate(row):
+            new_i = len(new_row)
             if i in ignore_cols and j != 0:
-                row[i] = 0 # meaning this was ignored
+                ignored_count += 1
+                continue # meaning this was ignored
             
             if has_labels and i == label_col:
+                new_row.append(element)
                 continue
             
             try:
-                row[i] = float(row[i])
+                float(element)
+                new_row.append(element)
             except:
                 if j: # if j != 0, which is where the column labels are
-                    row[i] = temp_cell = row[i].strip()
+                    temp_element = temp_cell = element.strip()
                     if temp_cell == '':
-                        row[i] = 0
-                        print "Warning: blank entry found in " + header[i] + ", 0 used instead"
+                        temp_element = 0
+                        print "Warning: blank entry found in " + header[i - ignored_head_count] + ", 0 used instead"
                     else:
-                        if not i in to_expand:
-                            to_expand.append(i)
+                        if not new_i in to_expand:
+                            to_expand.append(new_i)
                     
-        data.append(row)
+                    new_row.append(temp_element)
+                    
+        data.append(new_row)
 
-        
+
 # expand string data into different dimensions
 for i in to_expand:
     unique_strings = []
@@ -84,16 +100,21 @@ for i in to_expand:
                 build_row.append(0)
         
         row += build_row
-    unique_strings = map(lambda s: s+"_col_"+str(i), unique_strings)
+    unique_strings = map(lambda s: str(s)+"_col_"+header[i], unique_strings)
     data[0] += unique_strings
 
 to_expand.sort(reverse = True)
 
+
 # remove originals with strings from data
+
+#print to_expand
+#print len(data[1])
+
 for row in data:
     for j in to_expand:
         del row[j]
-
+        
 # add "row_{row num}" and key "md3_id"
 data[0] = ["md3_id"] + data[0]
 for i in range(1, len(data)):
@@ -103,7 +124,7 @@ for i in range(1, len(data)):
 for i, row in enumerate(data):
     if i < 2:
         pass#print row
-    
+
 # file name should only have one dot
 with open(datafile.split('.')[0]+'_clean.'+datafile.split('.')[1], 'wb') as fixed_file:
     fixed_writer = csv.writer(fixed_file, delimiter=',')
